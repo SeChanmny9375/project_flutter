2ff948930ffdb94fb79b4e835e37602dea5025eb
Se_Chanmny <pozzmny123@gamil.com>
2023-04-18 15:10:07 +0700

    update

--- lib/screen/welcome.dart
+++ lib/screen/welcome.dart
@@ -1,11 +1,12 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
 
 class Welcome extends StatelessWidget {
   const Welcome({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Placeholder(
+      child: Text('data'),
+    );
   }
 }
