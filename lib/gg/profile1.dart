7126eabfd8488824f67cddc4dbe4ab77abc43fdf
Se_Chanmny <pozzmny123@gamil.com>
2023-04-18 14:11:04 +0700

    added a new image to project.

---
+++ lib/gg/profile1.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Profile1 extends StatelessWidget {
+  const Profile1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
