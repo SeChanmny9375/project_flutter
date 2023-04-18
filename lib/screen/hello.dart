d6a9e0052adebbc06c9ea966c93646b37c074d38
Se_Chanmny <pozzmny123@gamil.com>
2023-04-18 14:57:58 +0700

    update

---
+++ lib/screen/hello.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Hello extends StatelessWidget {
+  const Hello({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
